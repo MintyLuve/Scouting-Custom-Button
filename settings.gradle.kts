f8946266eb8ccdb7d9af49d8923054f81de89328
Sophia <111466302+MintyLuve@users.noreply.github.com>
2023-11-08 19:06:04 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Scouting-Custom-Buttons"
+include(":app")
