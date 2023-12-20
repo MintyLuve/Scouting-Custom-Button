8ce62aa1fe25b7b2cc58cda1a068af359138c343
Sophia <111466302+MintyLuve@users.noreply.github.com>
2023-12-20 16:11:58 -0500

    woah stuuff

--- app/src/main/java/com/fro/scouting_custom_buttons/LovelyView.java
+++ app/src/main/java/com/fro/scouting_custom_buttons/LovelyView.java
@@ -5,27 +5,28 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class LovelyView extends View {
     //circle and text colors
-    private int circleCol, labelCol;
+    private int rectCol, labelCol;
     //label text
-    private String circleText;
+    private String rectText;
     //paint for drawing custom view
-    private Paint circlePaint;
+    private Paint rectPaint;
     public LovelyView(Context context, AttributeSet attrs){
         super(context, attrs);
         //paint object for drawing in onDraw
-        circlePaint = new Paint();
+        rectPaint = new Paint();
         //get the attributes specified in attrs.xml using the name we included
         TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.LovelyView, 0, 0);
 
         try {
             //get the text and colors specified using the names in attrs.xml
-            circleText = a.getString(R.styleable.LovelyView_circleLabel);
-            circleCol = a.getInteger(R.styleable.LovelyView_circleColor, 0);//0 is default
+            rectText = a.getString(R.styleable.LovelyView_rectLabel);
+            rectCol = a.getInteger(R.styleable.LovelyView_rectColor, 0);//0 is default
             labelCol = a.getInteger(R.styleable.LovelyView_labelColor, 0);
         } finally {
             a.recycle();
@@ -34,44 +35,33 @@ public class LovelyView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         //draw the View
-        //get half of the width and height as we are working with a circle
-        int viewWidthHalf = this.getMeasuredWidth()/2;
-        int viewHeightHalf = this.getMeasuredHeight()/2;
-        //get the radius as half of the width or height, whichever is smaller
-        //subtract ten so that it has some space around it
-        int radius = 0;
-        if(viewWidthHalf>viewHeightHalf){
-            radius=viewHeightHalf-10;}
-        else {
-            radius=viewWidthHalf-10;}
-        circlePaint.setStyle(Style.FILL);
-        circlePaint.setAntiAlias(true);
-        //set the paint color using the circle color specified
-        circlePaint.setColor(circleCol);
-        canvas.drawCircle(viewWidthHalf, viewHeightHalf, radius, circlePaint);
+        rectPaint.setStyle(Style.FILL);
+        rectPaint.setAntiAlias(true);
+
+        //set the paint color using the rect color specified
+        rectPaint.setColor(rectCol);
+        RectF rect = new RectF(0,0,600,600);
+        canvas.drawRoundRect(rect, 20, 20, rectPaint);
+
         //set the text color using the color specified
-        circlePaint.setColor(labelCol);
+        rectPaint.setColor(labelCol);
         //set text properties
-        circlePaint.setTextAlign(Paint.Align.CENTER);
-        circlePaint.setTextSize(50);
+        rectPaint.setTextAlign(Paint.Align.CENTER);
+        rectPaint.setTextSize(50);
         //draw the text using the string attribute and chosen properties
-        canvas.drawText(circleText, viewWidthHalf, viewHeightHalf, circlePaint);
+        canvas.drawText(rectText, 600, 600, rectPaint);
 
     }
 
-    public int getCircleColor(){
-        return circleCol;
+    public int getRectColor(){
+        return rectCol;
     }
-    public int getLabelColor(){
-        return labelCol;
-    }
-    public String getLabelText(){
-        return circleText;
-    }
+    public int getLabelColor(){return labelCol;}
+    public String getLabelText(){return rectText;}
 
-    public void setCircleColor(int newColor){
+    public void setRectColor(int newColor){
         //update the instance variable
-        circleCol=newColor;
+        rectCol=newColor;
         //redraw the view
         invalidate();
         requestLayout();
@@ -85,7 +75,7 @@ public class LovelyView extends View {
     }
     public void setLabelText(String newLabel){
         //update the instance variable
-        circleText=newLabel;
+        rectText=newLabel;
         //redraw the view
         invalidate();
         requestLayout();
--- app/src/main/java/com/fro/scouting_custom_buttons/MainActivity.java
+++ app/src/main/java/com/fro/scouting_custom_buttons/MainActivity.java
@@ -3,12 +3,18 @@ package com.fro.scouting_custom_buttons;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.graphics.Color;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private LovelyView myView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myView = (LovelyView)findViewById(R.id.custView);
+
     }
 }
